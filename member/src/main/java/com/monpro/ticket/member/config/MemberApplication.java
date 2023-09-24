9e28a11869d963586efcc573a83721554f792246
monpro <monproshen@gmail.com>
2023-09-24 15:37:32 +1000

    add log config

--- member/src/main/java/com/monpro/ticket/member/config/MemberApplication.java
+++ member/src/main/java/com/monpro/ticket/member/config/MemberApplication.java
@@ -1,15 +1,21 @@
 package com.monpro.ticket.member.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
+
 
 @SpringBootApplication
 @ComponentScan("com.monpro")
 public class MemberApplication {
 
+    private static final Logger log = LoggerFactory.getLogger(MemberApplication.class);
     public static void main(String[] args) {
-        SpringApplication.run(MemberApplication.class, args);
+        SpringApplication application = new SpringApplication(MemberApplication.class);
+        Environment environment = application.run(args).getEnvironment();
+        log.info("running on localhost:{}", environment.getProperty("server.port"));
     }
-
 }
