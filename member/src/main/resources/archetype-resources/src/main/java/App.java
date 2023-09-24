b0ba9ed5e92cfa7c51134e9d3cfca04c6d6da5df
monpro <monproshen@gmail.com>
2023-09-24 15:07:03 +1000

    init project

---
+++ member/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.monpro;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
