5abcb6e892493da6ebd0847a3594ecae3b822c54
monpro <monproshen@gmail.com>
2023-09-24 15:15:38 +1000

    add member module

--- member/src/main/java/com/monpro/ticket/member/controller/TestController.java
+++ member/src/main/java/com/monpro/ticket/member/controller/TestController.java
@@ -1,4 +1,4 @@
-package com.monpro.ticket;
+package com.monpro.ticket.member.controller;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,5 +8,5 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
     @GetMapping("/hello")
-    public String hello() {return "hellos1ss";}
+    public String hello() {return "hello!";}
 }
