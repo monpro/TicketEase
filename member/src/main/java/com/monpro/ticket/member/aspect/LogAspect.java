ceb9907cb3b1538e8700b2494691500e75ab52e9
monpro <monproshen@gmail.com>
2023-09-24 17:48:53 +1000

    add LogAspect to have more logs which would increase visibility

---
+++ member/src/main/java/com/monpro/ticket/member/aspect/LogAspect.java
@@ -0,0 +1,84 @@
+package com.monpro.ticket.member.aspect;
+
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.support.spring.PropertyPreFilters;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.multipart.MultipartFile;
+
+@Aspect
+@Component
+public class LogAspect {
+    public LogAspect() {
+       LOG.info("LogAspect");
+    }
+
+    private final static Logger LOG = LoggerFactory.getLogger(LogAspect.class);
+
+    /**
+     * 定义一个切点
+     */
+    @Pointcut("execution(public * com.monpro..*Controller.*(..))")
+    public void controllerPointcut() {
+    }
+
+    @Before("controllerPointcut()")
+    public void doBefore(JoinPoint joinPoint) {
+
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+        Signature signature = joinPoint.getSignature();
+        String name = signature.getName();
+
+        // 打印请求信息
+        LOG.info("------------- LOG -------------");
+        LOG.info("request url: {} {}", request.getRequestURL().toString(), request.getMethod());
+        LOG.info("class name: {}.{}", signature.getDeclaringTypeName(), name);
+        LOG.info("request remote address: {}", request.getRemoteAddr());
+
+        Object[] args = joinPoint.getArgs();
+
+        Object[] arguments = new Object[args.length];
+        for (int i = 0; i < args.length; i++) {
+            if (args[i] instanceof ServletRequest
+                    || args[i] instanceof ServletResponse
+                    || args[i] instanceof MultipartFile) {
+                continue;
+            }
+            arguments[i] = args[i];
+        }
+
+        String[] excludeProperties = {};
+        PropertyPreFilters filters = new PropertyPreFilters();
+        PropertyPreFilters.MySimplePropertyPreFilter excludefilter = filters.addFilter();
+        excludefilter.addExcludes(excludeProperties);
+        LOG.info("request parameters: {}", JSONObject.toJSONString(arguments, excludefilter));
+    }
+
+    @Around("controllerPointcut()")
+    public Object doAround(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        long startTime = System.currentTimeMillis();
+        Object result = proceedingJoinPoint.proceed();
+        String[] excludeProperties = {};
+        PropertyPreFilters filters = new PropertyPreFilters();
+        PropertyPreFilters.MySimplePropertyPreFilter excludefilter = filters.addFilter();
+        excludefilter.addExcludes(excludeProperties);
+        LOG.info("response: {}", JSONObject.toJSONString(result, excludefilter));
+        LOG.info("------------- time spent：{} ms -------------", System.currentTimeMillis() - startTime);
+        return result;
+    }
+
+}
