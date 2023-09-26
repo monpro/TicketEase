ff69f84144a52ff8223ea9526ae6b6d23e547afb
monpro <monproshen@gmail.com>
2023-09-26 17:42:20 +1000

    Add a POC version of register feature

--- member/src/main/java/com/monpro/ticket/member/controller/MemberController.java
+++ member/src/main/java/com/monpro/ticket/member/controller/MemberController.java
@@ -3,6 +3,7 @@ package com.monpro.ticket.member.controller;
 
 import com.monpro.ticket.member.service.MemberService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +20,11 @@ public class MemberController {
     @GetMapping("/count")
     public int count() {
         return memberService.count();
+    }
+
+    @PostMapping("/register")
+    public long register(String mobile) {
+        System.out.println("modile is" + mobile);
+        return memberService.register(mobile);
     }
 }
--- member/src/main/java/com/monpro/ticket/member/service/MemberService.java
+++ member/src/main/java/com/monpro/ticket/member/service/MemberService.java
@@ -1,19 +1,39 @@
 package com.monpro.ticket.member.service;
 
+import com.monpro.ticket.member.domain.Member;
+import com.monpro.ticket.member.domain.MemberExample;
 import com.monpro.ticket.member.mapper.MemberMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class MemberService {
-    private final MemberMapper mapper;
+    private final MemberMapper memberMapper;
 
     @Autowired
     public MemberService(MemberMapper mapper) {
-        this.mapper = mapper;
+        this.memberMapper = mapper;
     }
 
     public int count() {
-        return Math.toIntExact(mapper.countByExample(null));
+        return Math.toIntExact(memberMapper.countByExample(null));
+    }
+
+    public long register(String mobile) {
+        MemberExample memberExample = new MemberExample();
+        memberExample.createCriteria().andMobileEqualTo(mobile);
+        List<Member> members = memberMapper.selectByExample(memberExample);
+        if (!members.isEmpty()) {
+            throw new RuntimeException("Mobile has been registered");
+        } else {
+            Member member = new Member();
+            //TODO: use uuid in production
+            member.setId(System.currentTimeMillis());
+            member.setMobile(mobile);
+            memberMapper.insert(member);
+            return member.getId();
+        }
     }
 }
