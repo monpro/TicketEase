10b1d29af9f42e4bfc82465a042be2c2a7f63a48
monpro <monproshen@gmail.com>
2023-10-06 12:54:43 +1100

    add styling to LoginView component

--- web/src/views/LoginView.vue
+++ web/src/views/LoginView.vue
@@ -1,37 +1,41 @@
 <template>
-  <a-form
-    :model="formState"
-    name="basic"
-    :label-col="{ span: 8 }"
-    :wrapper-col="{ span: 16 }"
-    autocomplete="off"
-    @finish="onFinish"
-    @finishFailed="onFinishFailed"
-  >
-    <a-form-item
-      label="Username"
-      name="username"
-      :rules="[{ required: true, message: 'Please input your username!' }]"
-    >
-      <a-input v-model:value="formState.username" />
-    </a-form-item>
+  <a-row class="login">
+    <a-col :span="8" :offset="8" class="login-main">
+      <h1 style="text-align: center">Ticket System</h1>
+      <a-form
+        :model="formState"
+        name="basic"
+        autocomplete="off"
+        @finish="onFinish"
+        @finishFailed="onFinishFailed"
+      >
+        <a-form-item
+          label=""
+          name="mobile"
+          :rules="[{ required: true, message: 'Please input your mobile!' }]"
+        >
+          <a-input v-model:value="formState.mobile" placeholder="mobile" />
+        </a-form-item>
+        <a-form-item
+          label=""
+          name="code"
+          :rules="[
+            { required: true, message: 'Please input verification code!' },
+          ]"
+        >
+          <a-input v-model:value="formState.code" placeholder="code">
+            <template #addonAfter>
+              <a type="primary">get code</a>
+            </template>
+          </a-input>
+        </a-form-item>
 
-    <a-form-item
-      label="Password"
-      name="password"
-      :rules="[{ required: true, message: 'Please input your password!' }]"
-    >
-      <a-input-password v-model:value="formState.password" />
-    </a-form-item>
-
-    <a-form-item name="remember" :wrapper-col="{ offset: 8, span: 16 }">
-      <a-checkbox v-model:checked="formState.remember">Remember me</a-checkbox>
-    </a-form-item>
-
-    <a-form-item :wrapper-col="{ offset: 8, span: 16 }">
-      <a-button type="primary" html-type="submit">Submit</a-button>
-    </a-form-item>
-  </a-form>
+        <a-form-item>
+          <a-button type="primary" block html-type="submit">Submit</a-button>
+        </a-form-item>
+      </a-form>
+    </a-col>
+  </a-row>
 </template>
 
 <script>
@@ -39,9 +43,8 @@ import { defineComponent, reactive } from "vue";
 export default defineComponent({
   setup() {
     const formState = reactive({
-      username: "",
-      password: "",
-      remember: true,
+      mobile: "",
+      code: "",
     });
     const onFinish = (values) => {
       console.log("Success:", values);
@@ -58,4 +61,16 @@ export default defineComponent({
 });
 </script>
 
-<style></style>
+<style>
+.login-main h1 {
+  font-size: 25px;
+  font-weight: bold;
+}
+.login-main {
+  margin-top: 100px;
+  padding: 30px 30px 20px;
+  border: 2px solid grey;
+  border-radius: 10px;
+  background-color: #fcfcfc;
+}
+</style>
