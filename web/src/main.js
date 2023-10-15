89ddad3ef297e209e16d52825b49f3878e724f1d
monpro <monproshen@gmail.com>
2023-10-15 16:01:49 +1100

    add axios interceptors about request and response

--- web/src/main.js
+++ web/src/main.js
@@ -4,5 +4,27 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "ant-design-vue/dist/antd.css";
+import axios from "axios";
 
 createApp(App).use(Antd).use(store).use(router).mount("#app");
+
+axios.interceptors.request.use(
+  function (config) {
+    console.log("request config：", config);
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+axios.interceptors.response.use(
+  function (response) {
+    console.log("response：", response);
+    return response;
+  },
+  (error) => {
+    console.log("response error：", error);
+    return Promise.reject(error);
+  }
+);
